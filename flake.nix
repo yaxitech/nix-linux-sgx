3bc08604f51c8505e66e86692126747f69176df7
Vincent Haupert <mail@vincent-haupert.de>
2021-11-24 18:40:35 +0100

    Use github:veehaitch/nixpkgs/sgx-psw
    Also update nixpkgs to support the `hash` argument.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "YAXI Linux SGX packages and modules";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.nixpkgs-sgx.url = "github:veehaitch/nixpkgs/sgx-psw";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-sgx }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -12,57 +13,56 @@
       };
     in
     {
-      overlay = final: prev:
-        {
-          intel-sgx = final.callPackage ./pkgs/intel-sgx { };
-        };
+      overlay = final: prev: rec {
+        sgx-sdk = prev.callPackage "${nixpkgs-sgx}/pkgs/os-specific/linux/sgx/sdk" { };
+        sgx-psw = prev.callPackage "${nixpkgs-sgx}/pkgs/os-specific/linux/sgx/psw" { };
+        # Keep for compat
+        intel-sgx.sdk = sgx-sdk;
+        intel-sgx.psw = sgx-psw;
+      };
 
-      packages.${system} =
-        {
-          intel-sgx-sdk = pkgs.intel-sgx.sdk;
-          intel-sgx-psw = pkgs.intel-sgx.psw;
-        };
+      packages.${system} = {
+        intel-sgx-sdk = pkgs.intel-sgx.sdk;
+        intel-sgx-psw = pkgs.intel-sgx.psw;
+      };
 
-      nixosModules.sgx =
-        { ... }:
-        {
-          nixpkgs.overlays = [ self.overlay ];
-          imports = [ ./nixos/modules/sgx.nix ];
-        };
+      nixosModules.sgx = {
+        nixpkgs.overlays = [ self.overlay ];
+        imports = [ "${nixpkgs-sgx}/nixos/modules/security/sgx.nix" ];
+      };
 
       checks.${system}.nixpkgs-fmt = pkgs.runCommand "check-nix-format" { } ''
         ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
         mkdir $out #sucess
       '';
 
-      devShell.${system} = pkgs.mkShell
-        {
-          name = "linux-sgx-devshell";
+      devShell.${system} = pkgs.mkShell {
+        name = "linux-sgx-devshell";
 
-          buildInputs = with pkgs; [
-            intel-sgx.sdk
-            intel-sgx.psw
-            fish
-          ];
+        buildInputs = with pkgs; [
+          intel-sgx.sdk
+          intel-sgx.psw
+          fish
+        ];
 
-          SGX_SDK = "${pkgs.intel-sgx.sdk}";
-          SGX_PSW = "${pkgs.intel-sgx.psw}";
+        SGX_SDK = "${pkgs.intel-sgx.sdk}";
+        SGX_PSW = "${pkgs.intel-sgx.psw}";
 
-          shellHook = ''
-            source $SGX_SDK/share/bin/environment
+        shellHook = ''
+          source $SGX_SDK/share/bin/environment
 
-            export SGX_SDK_SAMPLES=$(mktemp -d)
-            cp --no-preserve=all -r $SGX_SDK/share/SampleCode $SGX_SDK_SAMPLES/
+          export SGX_SDK_SAMPLES=$(mktemp -d)
+          cp --no-preserve=all -r $SGX_SDK/share/SampleCode $SGX_SDK_SAMPLES/
 
-            echo "SGX_SDK         = $SGX_SDK"
-            echo "SGX_SDK_SAMPLES = $SGX_SDK_SAMPLES (rw)"
-            echo "SGX_PSW         = $SGX_PSW"
-            echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+          echo "SGX_SDK         = $SGX_SDK"
+          echo "SGX_SDK_SAMPLES = $SGX_SDK_SAMPLES (rw)"
+          echo "SGX_PSW         = $SGX_PSW"
+          echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 
-            trap "rm -rf $SGX_SDK_SAMPLES && echo 'Cleaned up samples dir'" EXIT
+          trap "rm -rf $SGX_SDK_SAMPLES && echo 'Cleaned up samples dir'" EXIT
 
-            exec fish
-          '';
-        };
+          exec fish
+        '';
+      };
     };
 }
